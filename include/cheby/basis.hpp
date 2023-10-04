63e76f10d4fe51a7156edcc81649b3f260541d76
Gwénaël Gabard <gwenael.gabard@univ-lemans.fr>
2023-10-04 10:39:10 +0200

    Add Chebyshev points of the two types

--- include/cheby/basis.hpp
+++ include/cheby/basis.hpp
@@ -1,43 +1,64 @@
-#ifndef CHEBY_Basis_H
-#define CHEBY_Basis_H
+#ifndef CHEBY_BASIS_H
+#define CHEBY_BASIS_H
 
-#include <vector>
-#include <cmath>
 #include "Eigen/Dense"
+#include <cmath>
 
 namespace cheby {
 
-template <typename valueT, typename parameterT=double, typename indexT=std::size_t>
+template <typename valueT, typename parameterT = double,
+          typename indexT = std::size_t>
 class Basis {
-public:
+  public:
     using Value = valueT;
     using Parameter = parameterT;
     using Index = indexT;
-    using ValVector = Eigen::Array<Value,Eigen::Dynamic,1>;
-    using ParamVector = Eigen::Array<Parameter,Eigen::Dynamic,1>;
-    
+    using ValVector = Eigen::Array<Value, Eigen::Dynamic, 1>;
+    using ParamVector = Eigen::Array<Parameter, Eigen::Dynamic, 1>;
+
     static constexpr double rel_tol = 1.e-14;
     static constexpr Index tail_length = 8;
-    
+
     Parameter xmin, xmax;
     Index order;
 
-    Basis(const Index degree, const Parameter start=-1.0, const Parameter end=+1.0) {
+    Basis(const Index degree, const Parameter start = -1.0,
+          const Parameter end = +1.0) {
         order = degree;
         xmin = start;
         xmax = end;
     };
 
-    const ParamVector points2(Index num_points=0) const {
+    const ParamVector points1(int num_points = -1) const {
         if (num_points == 0)
+            return ParamVector(0);
+        if (num_points < 0)
             num_points = order + 1;
-        ParamVector points = (Eigen::VectorXd::LinSpaced(num_points, 0, static_cast<double>(num_points)).array()*EIGEN_PI / num_points).cos();
-        points = (1.0 - points.array()) * 0.5 * (xmax - xmin) + xmin;
+        ParamVector points(num_points);
+        const Parameter a = EIGEN_PI / num_points;
+        const Parameter b = EIGEN_PI / (2.0 * num_points);
+        const Parameter c = (xmax + xmin) * 0.5;
+        const Parameter d = (xmin - xmax) * 0.5;
+        for (Index i = 0; i < num_points; ++i)
+            points(i) = c + d * std::cos(a * i + b);
+        return points;
+    };
+
+    const ParamVector points2(int num_points = -1) const {
+        if ((num_points == 0) || (num_points == 1))
+            return ParamVector(0);
+        if (num_points < 0)
+            num_points = order + 1;
+        ParamVector points(num_points);
+        const Parameter a = EIGEN_PI / (num_points - 1);
+        const Parameter b = (xmax + xmin) * 0.5;
+        const Parameter c = (xmin - xmax) * 0.5;
+        for (Index i = 0; i < num_points; ++i)
+            points(i) = b + c * std::cos(a * i);
         return points;
     };
 };
 
-
-}
+} // namespace cheby
 
 #endif
--- src/pybind11_wrapper.cpp
+++ src/pybind11_wrapper.cpp
@@ -13,7 +13,8 @@ using namespace cheby;
 void init_cheby(py::module &m){
     py::class_<Basis1D>(m, "Basis1D")
         .def(py::init<int, double, double>(), py::arg("order"), py::arg("start")=-1.0, py::arg("end")=1.0)
-        .def("points2", &Basis1D::points2, py::arg("num_points")=0);
+        .def("points1", &Basis1D::points1, py::arg("num_points")=-1)
+        .def("points2", &Basis1D::points2, py::arg("num_points")=-1);
 }
 
 
