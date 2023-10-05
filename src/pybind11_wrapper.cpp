3a91b948d558de622eca6b0ceab9da3bc208accc
Gwénaël Gabard <gwenael.gabard@univ-lemans.fr>
2023-10-05 10:20:52 +0200

    Add Python wrapper for real and complex functions

--- src/pybind11_wrapper.cpp
+++ src/pybind11_wrapper.cpp
@@ -15,6 +15,21 @@ void init_cheby(py::module &m){
         .def(py::init<int, double, double>(), py::arg("order"), py::arg("start")=-1.0, py::arg("end")=1.0)
         .def("points1", &Basis1D::points1, py::arg("num_points")=-1)
         .def("points2", &Basis1D::points2, py::arg("num_points")=-1);
+
+    py::class_<RealFunction>(m, "RealFunction")
+        .def(py::init<std::function<RealFunction::ValueVector(RealFunction::ParamVector)>, double, double, int>(), py::arg("f"), py::arg("start"), py::arg("end"), py::arg("N") = -1)
+        .def("__call__", &RealFunction::Eval)
+        .def("coef", &RealFunction::Coef)
+        .def("tail_length", &RealFunction::TailLength)
+        .def("trim", &RealFunction::Trim);
+
+    py::class_<ComplexFunction>(m, "ComplexFunction")
+        .def(py::init<std::function<ComplexFunction::ValueVector(ComplexFunction::ParamVector)>, double, double, int>(), py::arg("f"), py::arg("start"), py::arg("end"), py::arg("N") = -1)
+        .def("__call__", &ComplexFunction::Eval)
+        .def("coef", &ComplexFunction::Coef)
+        .def("tail_length", &ComplexFunction::TailLength)
+        .def("trim", &ComplexFunction::Trim);
+
 }
 
 
