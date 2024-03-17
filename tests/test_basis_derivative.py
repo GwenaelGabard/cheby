2eb509d16cbb24d6ef8c07833c06b94f2b642b73
Gwénaël Gabard <gwenael.gabard@univ-lemans.fr>
2024-03-17 14:18:00 +0100

    Add tests for basis derivatives

--- tests/test_basis_derivative.py
+++ tests/test_basis_derivative.py
@@ -1,51 +1,35 @@
+from itertools import product
 import numpy as np
 import pytest
-from cases import sub_vec, real_case_list, complex_case_list, x, fun_from_expr
-from cheby import RealFunction, ComplexFunction, Basis1D
+from cases import bound_list
+from cheby import Basis1D
 
 rel_tol = 1.0e-12
 abs_tol = 1.0e-12
 
-
-@pytest.mark.parametrize("fun, xmin, xmax", real_case_list)
-def test_real_diffmat(fun, xmin, xmax):
-    dfun = fun.diff()
-
-    ff = fun_from_expr(x, fun)
-    dff = fun_from_expr(x, dfun)
-
-    f = RealFunction(ff, xmin, xmax)
-    df_ex = RealFunction(dff, xmin, xmax)
-    basis = Basis1D(len(f.coef()) - 1, xmin, xmax)
-    c_num = basis.diff_matrix() @ f.coef()
-
-    delta = sub_vec(df_ex.coef(), c_num)
-    error = np.max(np.abs(delta))
-    norm = np.max(np.abs(df_ex.coef()))
-
-    if norm == 0.0:
-        assert error < abs_tol
-    else:
-        assert error / norm < rel_tol
+order_list = [2, 3, 4, 5, 6, 7, 8, 9, 10, 13, 25, 34, 123]
+degree_list = [1, 2, 3, 4, 5]
 
 
-@pytest.mark.parametrize("fun, xmin, xmax", real_case_list + complex_case_list)
-def test_complex_diffmat(fun, xmin, xmax):
-    dfun = fun.diff()
+@pytest.mark.parametrize(
+    "order, bounds, degree", product(order_list, bound_list, degree_list)
+)
+def test_basis_derivative(order, bounds, degree):
+    basis = Basis1D(order, bounds[0], bounds[1])
+    xi = np.linspace(-1, 1, 56)
+    x = (xi + 1) / 2.0 * (bounds[1] - bounds[0]) + bounds[0]
+    V_ex = basis.eval(x)
+    dV = basis.derivatives(x, degree)
+    D = basis.diff_matrix()
 
-    ff = fun_from_expr(x, fun)
-    dff = fun_from_expr(x, dfun)
+    for d in range(degree + 1):
+        V = dV[d]
+        error = np.max(np.abs(V - V_ex))
+        norm = np.max(np.abs(V_ex))
 
-    f = ComplexFunction(ff, xmin, xmax)
-    df_ex = ComplexFunction(dff, xmin, xmax)
-    basis = Basis1D(len(f.coef()) - 1, xmin, xmax)
-    c_num = basis.diff_matrix() @ f.coef()
+        if norm == 0.0:
+            assert error < abs_tol
+        else:
+            assert error / norm < rel_tol
 
-    delta = sub_vec(df_ex.coef(), c_num)
-    error = np.max(np.abs(delta))
-    norm = np.max(np.abs(df_ex.coef()))
-
-    if norm == 0.0:
-        assert error < abs_tol
-    else:
-        assert error / norm < rel_tol
+        V_ex = V_ex @ D
---
+++ tests/test_basis_diffmatrix.py
@@ -0,0 +1,51 @@
+import numpy as np
+import pytest
+from cases import sub_vec, real_case_list, complex_case_list, x, fun_from_expr
+from cheby import RealFunction, ComplexFunction, Basis1D
+
+rel_tol = 1.0e-12
+abs_tol = 1.0e-12
+
+
+@pytest.mark.parametrize("fun, xmin, xmax", real_case_list)
+def test_real_diffmat(fun, xmin, xmax):
+    dfun = fun.diff()
+
+    ff = fun_from_expr(x, fun)
+    dff = fun_from_expr(x, dfun)
+
+    f = RealFunction(ff, xmin, xmax)
+    df_ex = RealFunction(dff, xmin, xmax)
+    basis = Basis1D(len(f.coef()) - 1, xmin, xmax)
+    c_num = basis.diff_matrix() @ f.coef()
+
+    delta = sub_vec(df_ex.coef(), c_num)
+    error = np.max(np.abs(delta))
+    norm = np.max(np.abs(df_ex.coef()))
+
+    if norm == 0.0:
+        assert error < abs_tol
+    else:
+        assert error / norm < rel_tol
+
+
+@pytest.mark.parametrize("fun, xmin, xmax", real_case_list + complex_case_list)
+def test_complex_diffmat(fun, xmin, xmax):
+    dfun = fun.diff()
+
+    ff = fun_from_expr(x, fun)
+    dff = fun_from_expr(x, dfun)
+
+    f = ComplexFunction(ff, xmin, xmax)
+    df_ex = ComplexFunction(dff, xmin, xmax)
+    basis = Basis1D(len(f.coef()) - 1, xmin, xmax)
+    c_num = basis.diff_matrix() @ f.coef()
+
+    delta = sub_vec(df_ex.coef(), c_num)
+    error = np.max(np.abs(delta))
+    norm = np.max(np.abs(df_ex.coef()))
+
+    if norm == 0.0:
+        assert error < abs_tol
+    else:
+        assert error / norm < rel_tol
