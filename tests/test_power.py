acff9d963dd79ad75f761ef6c3a6284f972186b9
Gwénaël Gabard <gwenael.gabard@univ-lemans.fr>
2026-08-05 17:37:55 +0200

    Add tests for the power operator

--- tests/test_power.py
+++ tests/test_power.py
@@ -38,3 +38,35 @@ def test_power_complex(fun, xmin, xmax, order):
     norm = np.max(np.abs(f_ex.coef))
 
     assert error <= abs_tol + rel_tol * norm
+
+
+@pytest.mark.parametrize("fun, xmin, xmax", real_case_list)
+@pytest.mark.parametrize("order", order_list)
+def test_power_operator_real(fun, xmin, xmax, order):
+    ff = fun_from_expr(x, fun)
+    f = RealFunction(ff, xmin, xmax) ** order
+
+    ffn = fun_from_expr(x, fun**order)
+    f_ex = RealFunction(ffn, xmin, xmax)
+
+    delta = sub_vec(f_ex.coef, f.coef)
+    error = np.max(np.abs(delta))
+    norm = np.max(np.abs(f_ex.coef))
+
+    assert error <= abs_tol + rel_tol * norm
+
+
+@pytest.mark.parametrize("fun, xmin, xmax", real_case_list + complex_case_list)
+@pytest.mark.parametrize("order", order_list)
+def test_power_operator_complex(fun, xmin, xmax, order):
+    ff = fun_from_expr(x, fun)
+    f = ComplexFunction(ff, xmin, xmax) ** order
+
+    ffn = fun_from_expr(x, fun**order)
+    f_ex = ComplexFunction(ffn, xmin, xmax)
+
+    delta = sub_vec(f_ex.coef, f.coef)
+    error = np.max(np.abs(delta))
+    norm = np.max(np.abs(f_ex.coef))
+
+    assert error <= abs_tol + rel_tol * norm
